d0223d4ca6f799d4fda779f96e03855bb11d4e39
Salman-D <87569242+Salman-D@users.noreply.github.com>
2021-10-17 16:20:20 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 function fb_logim(){
-    alert("salman")
+  
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+        .then((res)=>{
+            console.log(res.user)
+                    console.log(res.user.email)
+                    console.log(res.user.DisplayName)
+                    console.log(res.photoURL)
+        })
+        .catch((err)=>{
+        console.log(err)
+        })
+        console.log(provider)
+    
 }
